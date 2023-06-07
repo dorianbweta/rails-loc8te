b831d0937cd0fdf2270b9922efbc9a3b3754c500
Giovanny Hidalgo <ghvcaslv@gmail.com>
2023-06-06 21:16:45 -0700

    removed skip before action authenticate user

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,7 +1,6 @@
 class TripsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index ]
   def index
-    @trips = Trip.all
+    @trips = Trip.where(user_id: current_user.id)
   end
 
   def new
