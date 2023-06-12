bf3ccedb8778fcd939ccec139b0e095b12057dcb
Prisillia Woe <prisilliawoe@gmail.com>
2023-06-12 10:15:24 +0800

    temp changes

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -3,5 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="navbar"
 export default class extends Controller {
   connect() {
+    console.log("heyheyehyehe from navbar");
   }
 }
