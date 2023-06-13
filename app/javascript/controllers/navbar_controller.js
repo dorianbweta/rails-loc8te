d0578ec70f0b26ac80cf3f27e03e3180af29d10b
Prisillia Woe <prisilliawoe@gmail.com>
2023-06-13 09:30:30 +0800

    Javascript for navbar and splash page

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,7 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navbar"
 export default class extends Controller {
-  connect() {
-    console.log("heyheyehyehe from navbar");
+  static targets = ["burger", "nav"]
+
+  openMenu(e) {
+    this.burgerTarget.classList.toggle('is-open');
+    this.navTarget.classList.toggle('is-open');
   }
 }
