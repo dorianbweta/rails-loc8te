a6b2d18a4300921c83d2a1fe06a4dd7f055c2535
Giovanny Hidalgo <ghvcaslv@gmail.com>
2023-06-12 20:08:38 -0700

    adding column and changed some categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_12_015351) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_13_030613) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,6 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_12_015351) do
     t.float "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_path"
   end
 
   create_table "rides", force: :cascade do |t|
