76c0085f1543bd80cad9205fb2995e6436538181
Prisillia Woe <prisilliawoe@gmail.com>
2023-06-15 15:44:56 +0800

    updated homepage

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,5 +2,6 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
-import {transitionItem} from "./transition"
-transitionItem();
+import {sliderItem} from "./slider"
+
+sliderItem();
